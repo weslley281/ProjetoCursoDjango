3a69688d06171523f5d387364a8c29725933e362
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-03-24 17:06:43 -0400

    a

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html")
+    return render(request, "recipes/home.html", context={
+        'name': 'Weslley Ferraz'
+    })
 
 def about(request):
     return HttpResponse('ABOUT')

060ee127d92596a8bc2de268db4bf9f59117cc99
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-03-24 16:44:36 -0400

    a

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "recipes/home.html")
 
 def about(request):
     return HttpResponse('ABOUT')
